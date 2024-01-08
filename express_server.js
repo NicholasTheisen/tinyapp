562fad5cafde29ecde2a4f06aaf9080b4e0360b1
NicholasTheisen <nicholas.theisen1997@gmail.com>
2024-01-08 03:41:31 +0000

    Added functionality for URL Shortening

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,9 +31,24 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars); // urls_index.ejs
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
   let id = req.params.id;
   let longURL = urlDatabase[id];
   let templateVars = { id: id, longURL: longURL };
   res.render("urls_show", templateVars);
 });
+
+function generateRandomString() {
+  let randomString = Math.random().toString(36).substring(2,8);
+  return randomString;
+};
+
